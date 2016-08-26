f32fbd832570e79375448352ca5db07c1b3fed77
carolina mendoza <pumita1893@gmail.com>
2016-08-25 23:47:02 -0500

    Tarea 2 con reporte

---
+++ Ejercicio2/src/GLApplication.cpp
@@ -0,0 +1,189 @@
+
+/*	Ejercicio 2
+	Elaborado por Mendoza Saucedo Carolina
+	*/
+
+#include "Headers/GLApplication.h"
+
+// Shaders
+const GLchar* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"}\0";
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+//"color = vec4(0.3f, 0.6f, 0.9f, 1.0f);\n"
+"color = vec4(1.0f, 0.0f, 1.0f, 1.0f);\n"
+"}\n\0";
+
+GLuint VBO1, VBO2, VAO1, VAO2, EBO;
+GLint vertexShader, fragmentShader, shaderProgram;
+typedef struct {
+	float XYZ[3];
+} Vertices;
+
+
+GLApplication::GLApplication() :
+windowManager(nullptr) {
+}
+
+GLApplication::~GLApplication() {
+	destroy();
+}
+
+void GLApplication::GLMain() {
+	initialize();
+	applicationLoop();
+}
+
+void GLApplication::initialize() {
+	if (!windowManager
+		|| !windowManager->initialize(800, 700, "Ejercicio2", false)) {
+		this->destroy();
+		exit(-1);
+	}
+
+	glViewport(0, 0, WindowManager::screenWidth, WindowManager::screenHeight);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	// Build and compile our shader program
+	// Vertex shader
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// Check for compile time errors
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog
+			<< std::endl;
+	}
+	// Fragment shader
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// Check for compile time errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog
+			<< std::endl;
+	}
+	// Link shaders
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// Check for linking errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog
+			<< std::endl;
+	}
+
+	GLfloat vertices1[] = { -0.5f, 0.5f, 0.0f,
+							-1.0f, -0.5f, 0.0f,
+							0.0f, -0.5f, 0.0f
+	};
+
+	GLfloat vertices2[] = {
+							0.0f, -0.5f, 0.0f,
+							1.0f, -0.5f, 0.0f,
+							0.5f, 0.5f, 0.0f
+	};
+
+	glGenVertexArrays(1, &VAO1);
+	glGenBuffers(1, &VBO1);
+	glBindVertexArray(VAO1);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
+		(GLvoid*)0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
+
+	glGenVertexArrays(1, &VAO2);
+	glGenBuffers(1, &VBO2);
+	glBindVertexArray(VAO2);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
+		(GLvoid*)0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
+	/*
+	
+	glGenBuffers(1, &VBO2);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
+		(GLvoid*)0);
+	
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);*/
+}
+
+void GLApplication::applicationLoop() {
+	bool processInput = true;
+	while (processInput) {
+		processInput = windowManager->processInput(true);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO1);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		
+		glBindVertexArray(0);
+
+		glBindVertexArray(VAO2);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glBindVertexArray(0);
+		windowManager->swapTheBuffers();
+	}
+}
+
+void GLApplication::destroy() {
+	if (windowManager) {
+		delete windowManager;
+		windowManager = nullptr;
+	}
+
+	glUseProgram(0);
+
+	glDetachShader(shaderProgram, vertexShader);
+	glDetachShader(shaderProgram, fragmentShader);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	glDeleteProgram(shaderProgram);
+
+	glDisableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glDeleteBuffers(1, &VBO1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glDeleteBuffers(1, &VBO2);
+
+	glBindVertexArray(0);
+	glDeleteVertexArrays(1, &VAO1);
+	glDeleteVertexArrays(1, &VAO2);
+}
